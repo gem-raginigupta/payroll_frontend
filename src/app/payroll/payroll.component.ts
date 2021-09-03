5a4695dba8f6fa7fce6c0af2c07f6666530d6baf
uj.sharma <ujjwal.sharma@geminisolutions.in>
2021-09-03 20:33:41 +0530

    Adding static view of Payroll page

--- src/app/payroll/payroll.component.ts
+++ src/app/payroll/payroll.component.ts
@@ -20,11 +20,10 @@ import { EmployeeService} from '../shared/services/employee.service';
       }
       get_empDetails() {
         this.empListSourceRaw = [
-          {'GEM_ID': 'GSI G 001','Name': 'Den Cooper', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 002','Name': 'Eric Cooper', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 003','Name': 'Paul Cooper', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 004','Name': 'Kevin Malone', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 005','Name': 'Michael Scott', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'}
+          {'GEM_ID': 'GSI G 001','Name': 'Michael Scott', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
+          {'GEM_ID': 'GSI G 002','Name': 'Dwight Shrute', 'Department': 'Android Dev', 'Role': 'Assistant to the Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
+          {'GEM_ID': 'GSI G 003','Name': 'Creed Bratton', 'Department': 'Quality Assurance', 'Role': 'Senior Developer', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
+          {'GEM_ID': 'GSI G 004','Name': 'Kevin Malone', 'Department': 'Accounts', 'Role': 'Accountant', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'}
         ]
         this.empListSource = new MatTableDataSource<any>(this.empListSourceRaw)
         console.log(this.empListSource)
