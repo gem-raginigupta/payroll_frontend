08cc66570913c09f32eea35c108ba90f387c35c0
uj.sharma <ujjwal.sharma@geminisolutions.in>
2021-09-07 22:38:03 +0530

    Intergrating Payroll screen to the Backend

--- src/app/payroll/payroll.component.ts
+++ src/app/payroll/payroll.component.ts
@@ -1,31 +1,63 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatPaginator, MatSort, MatTableDataSource, MatTableModule } from '@angular/material';
-import { EmployeeService} from '../shared/services/employee.service';
+import { EmployeeService } from '../shared/services/employee.service';
 
 @Component({
-    selector: 'payroll',
-    templateUrl: './payroll.component.html',
-    styleUrls: ['./payroll.component.css']
-  })
+  selector: 'payroll',
+  templateUrl: './payroll.component.html',
+  styleUrls: ['./payroll.component.css']
+})
 
 
-  export class PayrollComponent{
-      empListSourceRaw: any;
-      public empListSource: MatTableDataSource<any>;
-      displayedColumns: string[] = ['gem_id', 'name', 'dept', 'role', 'total_salary', 'payment_status'];
-      @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-      @ViewChild(MatSort, {static: true}) sort: MatSort;
-      ngOnInit() {
-        this.get_empDetails();
-      }
-      get_empDetails() {
-        this.empListSourceRaw = [
-          {'GEM_ID': 'GSI G 001','Name': 'Michael Scott', 'Department': 'Android Dev', 'Role': 'Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 002','Name': 'Dwight Shrute', 'Department': 'Android Dev', 'Role': 'Assistant to the Executive Manger', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 003','Name': 'Creed Bratton', 'Department': 'Quality Assurance', 'Role': 'Senior Developer', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'},
-          {'GEM_ID': 'GSI G 004','Name': 'Kevin Malone', 'Department': 'Accounts', 'Role': 'Accountant', 'Total_Salary': 'Rs. 655,733.00', 'Payment_status': 'Paid'}
-        ]
-        this.empListSource = new MatTableDataSource<any>(this.empListSourceRaw)
-        console.log(this.empListSource)
-      }
+export class PayrollComponent {
+  empListSourceRaw: any;
+  constructor(public dialog: MatDialog, private employeeService: EmployeeService) { }
+  public empListSource: MatTableDataSource<any>;
+  allEmpDetails: any;
+  empListDataSource: any;
+  allPayrollDetails: any;
+  payrollDataParsed: any;
+  displayedColumns: string[] = ['gem_id', 'name', 'dept', 'role', 'total_salary', 'payment_status'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  ngOnInit() {
+    this.getAllEmployeesDetails();
+    this.getAllPayrollDetails();
   }
+  getAllPayrollDetails() {
+    this.employeeService.getAllPayrollDetailsApi().subscribe(
+      res => {
+        this.allPayrollDetails = res.data;
+        this.payrollDataParsed = this.parsePayrollData(res.data);
+        console.log(this.allPayrollDetails);
+        console.log('Parsed', this.payrollDataParsed);
+      },
+      error => {
+        console.log('get all payroll details failed', error);
+      }
+    );
+  }
+  getAllEmployeesDetails() {
+    this.employeeService.getAllEmployeesDetailsApi().subscribe(
+      res => {
+        this.allEmpDetails = res.data;
+        this.empListDataSource = new MatTableDataSource<any>(this.allEmpDetails);
+        this.empListDataSource.sort = this.sort;
+        this.empListDataSource.paginator = this.paginator;
+        console.log('get all emp details res', this.allEmpDetails);
+      },
+      error => {
+        console.log('get all emp details failed', error);
+      }
+    );
+  }
+  parsePayrollData(payrollData) {
+    this.payrollDataParsed = {}
+    for (let i = 0; i < payrollData.length; i++){
+      if(payrollData[i].component == 'BASIC_PAY'){
+        this.payrollDataParsed[payrollData[i].employeeId] = payrollData[i].value
+      }
+    }
+    return this.payrollDataParsed;
+  }
+}
