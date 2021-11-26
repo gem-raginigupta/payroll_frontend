8fb6589207560dede0bd6950d4ddc1c9bb2300b7
gem-raginigupta <ragini.gupta@geminisolutions.in>
2021-11-26 15:14:15 +0530

    changed configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   baseURL: 'https://gempayrolldevapi.geminisolutions.com/',
   clientId: '20734553973-bsgtlohdevej8c6s0tttnjd7icugolam.apps.googleusercontent.com'
 };
