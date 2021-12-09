982d956cd476c603aa698f0c48edc1836eba645a
uj.sharma <ujjwal.sharma@geminisolutions.in>
2021-12-09 18:59:52 +0530

    Changes  in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseURL: 'https://gempayrollapi.geminisolutions.com/',
+  baseURL: 'https://gempayrolldevapi.geminisolutions.com/',
   clientId: '20734553973-qp8nktlbrblh95i5h7tric4jt6da4d30.apps.googleusercontent.com',
   env: 'local'
 };
