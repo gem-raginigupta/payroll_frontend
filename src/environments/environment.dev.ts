db884edfe5771b4912d16c4828ab75594bc3f51c
gem-raginigupta <ragini.gupta@geminisolutions.in>
2021-12-10 23:35:47 +0530

    changed client id dev env file

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseURL: 'https://gempayrolldevapi.geminisolutions.com/',
-  clientId: '20734553973-qp8nktlbrblh95i5h7tric4jt6da4d30.apps.googleusercontent.com',
+  clientId: '923610267740-fh42m58rsmfv3b94c2t7k46ad3q79c5g.apps.googleusercontent.com',
   env: 'dev'
 };
