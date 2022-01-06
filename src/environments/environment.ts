bdd82a00c442677e81e77e44c36dcd42c59332d4
uj.sharma <ujjwal.sharma@geminisolutions.in>
2022-01-06 19:32:43 +0530

    Changing environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseURL: 'https://gempayrolldevapi.geminisolutions.com/',
-  clientId: '923610267740-fh42m58rsmfv3b94c2t7k46ad3q79c5g.apps.googleusercontent.com',
+  clientId: '923610267740-m6is6fppdvke8jejn87benr27hlm134n.apps.googleusercontent.com',
   env: 'local'
 };
 
