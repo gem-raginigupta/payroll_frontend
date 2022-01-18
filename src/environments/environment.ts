b0217578804b1ea6c4aac81f7d941894f3ec55e1
gem-raginigupta <83813583+gem-raginigupta@users.noreply.github.com>
2022-01-18 18:30:38 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseURL: 'https://gempayrolldevapi.geminisolutions.com/',
+  production: true,
+  baseURL: 'https://gempayrollapi.geminisolutions.com/',
   clientId: '923610267740-m6is6fppdvke8jejn87benr27hlm134n.apps.googleusercontent.com',
   env: 'local'
 };
