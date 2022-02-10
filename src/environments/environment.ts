53d9f50feed782547628c4eb058d55264501f5b6
ujsharma-gem <89975420+ujsharma-gem@users.noreply.github.com>
2022-02-10 19:44:15 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  baseURL: 'https://gempayrollapi.geminisolutions.com/',
-  clientId: '923610267740-m6is6fppdvke8jejn87benr27hlm134n.apps.googleusercontent.com',
-  env: 'local'
+  baseURL: 'https://gempayrolldevapi.geminisolutions.com/',
+  clientId: '923610267740-fh42m58rsmfv3b94c2t7k46ad3q79c5g.apps.googleusercontent.com',
+  env: 'dev'
 };
 
 /*
